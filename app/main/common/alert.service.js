06d9cdc2c093e4ded560e13bf87525dc9e9c24c1
Shalitha Senarathne <shalitha89@gmail.com>
2017-01-23 13:51:41 +0800

    warning alert added

--- app/main/common/alert.service.js
+++ app/main/common/alert.service.js
@@ -2,51 +2,59 @@
  * Created by shalitha on 23/5/16.
  */
 angular.module('itouch.services')
-.service('Alert', ['$ionicLoading', '$ionicPopup', function ($ionicLoading, $ionicPopup) {
-  this.showLoading = function () {
-    $ionicLoading.show({
-      template: '<ion-spinner icon="lines"></ion-spinner>',
-      noBackdrop: false
-    });
-  };
+  .service('Alert', ['$ionicLoading', '$ionicPopup', function ($ionicLoading, $ionicPopup) {
+    this.showLoading = function () {
+      $ionicLoading.show({
+        template: '<ion-spinner icon="lines"></ion-spinner>',
+        noBackdrop: false
+      });
+    };
 
-  this.hideLoading = function () {
-    $ionicLoading.hide();
-  };
+    this.hideLoading = function () {
+      $ionicLoading.hide();
+    };
 
-  this.CLASS_WARNING = 'energized';
-  this.CLASS_ERROR = 'assertive';
-  this.CLASS_INFO = 'calm';
+    this.CLASS_WARNING = 'energized';
+    this.CLASS_ERROR = 'assertive';
+    this.CLASS_INFO = 'calm';
 
-  this.showAlert = function (title, message, cssClass) {
-    return $ionicPopup.alert({
-      cssClass: cssClass || null,
-      title: title,
-      template: message
-    });
-  }
+    this.showAlert = function (title, message, cssClass) {
+      return $ionicPopup.alert({
+        cssClass: cssClass || null,
+        title: title,
+        template: message
+      });
+    }
 
-  this.success = function (message, title) {
-    return $ionicPopup.alert({
-      // cssClass: cssClass || null,
-      title: title,
-      template: message
-    });
-  }
+    this.success = function (message, title) {
+      return $ionicPopup.alert({
+        // cssClass: cssClass || null,
+        title: title,
+        template: message
+      });
+    }
 
     this.error = function (message, title) {
-        return $ionicPopup.alert({
-            // cssClass: cssClass || null,
-            title: title || 'Error',
-            template: message
-        });
+      return $ionicPopup.alert({
+        // cssClass: cssClass || null,
+        title: title || 'Error',
+        template: message
+      });
+    }
+
+    this.warning = function (message, title) {
+      return $ionicPopup.alert({
+        // cssClass: cssClass || null,
+        title: title || 'Warning!',
+        template: message
+      });
     }
 
 
-    this.showConfirm = function(message, title){
-    return $ionicPopup.confirm({
-      title: title,
-      template: message
-    });
-  }
-}]);
+    this.showConfirm = function (message, title) {
+      return $ionicPopup.confirm({
+        title: title,
+        template: message
+      });
+    }
+  }]);
