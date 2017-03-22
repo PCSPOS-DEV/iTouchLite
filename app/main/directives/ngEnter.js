0fde4fc3e724d6080acde6d31c8eeb6b1852386c
Shalitha Senarathne <shalitha89@gmail.com>
2017-03-22 11:40:42 +0800

    en enter directive added

---
+++ app/main/directives/ngEnter.js
@@ -0,0 +1,28 @@
+'use strict';
+angular.module('itouch')
+
+  .directive('ngEnter', ['$log', function($log) {
+    return function(scope, elements, attrs) {
+      elements.bind('keydown keypress', function(event) {
+        // $log.log(event.which);
+        if(13 === event.which) {
+          // console.log('enter');
+          scope.$apply(function() {
+            scope.$eval(attrs.ngEnter);
+            event.preventDefault();
+          });
+        }
+      });
+    };
+  }])
+
+  // .directive('ngScan', function() {
+  //   return function(scope, elements, attrs) {
+  //     elements.bind('change', function(event) {
+  //       console.log('change');
+  //       scope.$apply(function() {
+  //         scope.$eval(attrs.ngScan);
+  //       });
+  //     });
+  //   };
+  // });
