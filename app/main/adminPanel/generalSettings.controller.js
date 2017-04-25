45f36188d44ffc8904e7086098637a2faad10adf
Shalitha Senarathne <shalitha89@gmail.com>
2017-04-25 10:09:21 +0800

    settings issue fixed

--- app/main/adminPanel/generalSettings.controller.js
+++ app/main/adminPanel/generalSettings.controller.js
@@ -5,6 +5,7 @@ angular.module('itouch.controllers')
   .controller("GeneralSettingsCtrl", ['$scope', 'SettingsService', '$state', 'SyncService', 'AppConfig', 'Restangular', '$q', 'Alert',
       function ($scope, SettingsService, $state, SyncService, AppConfig, Restangular, $q, Alert) {
     var self = this;
+          self.settings = {};
 
     $scope.$on('viewOpen', function(event, data){
       if(data == 'general'){
@@ -24,65 +25,57 @@ angular.module('itouch.controllers')
           outletServerUrl: AppConfig.getOutletServerUrl(),
         };
       } catch(ex){
-        self.settings = {
-          ent_id:  null,
-          loc_id:  null,
-          mac_id: null
-        };
+        self.settings.ent_id = null;
+        self.settings.loc_id = null;
+        self.settings.mac_id = null;
+
         console.log(ex);
       }
     }
     refresh();
 
     self.save = function () {
-      var statusChecks = {};
-        if(!_.isUndefined(self.settings.url) && !_.isNull(self.settings.url)){
-            statusChecks.baseUrl = checkStatus(self.settings.url).then(function () {
+        if(!_.isUndefined(self.settings.url) && !_.isNull(self.settings.url) && !_.isEmpty(self.settings.url)){
+            checkStatus(self.settings.url).then(function () {
                 Restangular.setBaseUrl(self.settings.url);
                 AppConfig.setBaseUrl(self.settings.url);
+
+                if(_.isUndefined(self.settings.ent_id) || _.isNull(self.settings.ent_id)){
+                    return false;
+                }
+                if(_.isUndefined(self.settings.loc_id) || _.isNull(self.settings.loc_id)){
+                    return false;
+                }
+                if(_.isUndefined(self.settings.mac_id) || _.isNull(self.settings.mac_id)){
+                    return false;
+                }
+
+                SettingsService.setEntityId(self.settings.ent_id);
+                SettingsService.setLocationId(self.settings.loc_id);
+                SettingsService.setMachineId(self.settings.mac_id);
+                SettingsService.setCashId(self.settings.cash_id);
+                SettingsService.save();
+
+                SyncService.do().then(function () {
+                    SettingsService.setEntityId(self.settings.ent_id);
+                    SettingsService.setLocationId(self.settings.loc_id);
+                    SettingsService.setMachineId(self.settings.mac_id);
+                    SettingsService.save();
+                });
                 return true;
             }, function () {
                 Alert.warning('Invalid base url entered');
             });
         }
 
-        if(!_.isUndefined(self.settings.outletServerUrl) && !_.isNull(self.settings.outletServerUrl)){
-            statusChecks.outletServerUrl = checkStatus(self.settings.outletServerUrl).then(function () {
+        if(!_.isUndefined(self.settings.outletServerUrl) && !_.isNull(self.settings.outletServerUrl) && !_.isEmpty(self.settings.outletServerUrl)){
+            checkStatus(self.settings.outletServerUrl).then(function () {
                 AppConfig.setOutletServerUrl(self.settings.outletServerUrl);
                 return true;
             }, function () {
                 Alert.warning('Invalid outlet server url entered');
             });
         }
-      $q.all(statusChecks).finally(function (fin) {
-
-          refresh();
-          if(_.isUndefined(self.settings.url) || _.isNull(self.settings.url)){
-            return false;
-          }
-          if(_.isUndefined(self.settings.ent_id) || _.isNull(self.settings.ent_id)){
-              return false;
-          }
-          if(_.isUndefined(self.settings.loc_id) || _.isNull(self.settings.loc_id)){
-              return false;
-          }
-          if(_.isUndefined(self.settings.mac_id) || _.isNull(self.settings.mac_id)){
-              return false;
-          }
-
-          SettingsService.setEntityId(self.settings.ent_id);
-          SettingsService.setLocationId(self.settings.loc_id);
-          SettingsService.setMachineId(self.settings.mac_id);
-          SettingsService.setCashId(self.settings.cash_id);
-          SettingsService.save();
-
-          SyncService.do().then(function () {
-              SettingsService.setEntityId(self.settings.ent_id);
-              SettingsService.setLocationId(self.settings.loc_id);
-              SettingsService.setMachineId(self.settings.mac_id);
-              SettingsService.save();
-          });
-      });
 
 
     }
