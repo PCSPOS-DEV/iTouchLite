95f7323c44109d70ee2767e2f69c6fa00432236b
Lynn Naing <lynnnaing@prima.com.sg>
2018-02-21 11:15:47 +0800

    Sales Kits : Child Selections Fix Alpha
    Sales Kits : Child Selections Fix Alpha
    Need Testing

--- app/main/salesKits/salesKit.service.js
+++ app/main/salesKits/salesKit.service.js
@@ -205,13 +205,14 @@ angular.module('itouch.services')
                       }
 
                       angular.forEach(selections, function (skisl) {
+                        if (skisl.SalesKitItemsId != undefined) {
+                          skisl.ItemId = parseInt(skisl.ItemId + String(skisl.SalesKitItemsId));
+                        }
                         salesKit.list[skisl.ItemId] = skisl;
                         salesKit.list[skisl.ItemId].Qty = 0;
                         salesKit.list[skisl.ItemId].componetid = ski.ItemId;
                         salesKit.list[skisl.ItemId].QtyValid = skisl.Quantity;
                       });
-
-                      // console.log(selections);
                       return selections;
                     }));
 
