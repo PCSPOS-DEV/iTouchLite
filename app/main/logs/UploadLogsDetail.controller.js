dac8f0a6469c58bd0b355d26cf4afc9e384a7ce6
Lynn Naing Zaw <lynnnaing@prima.com.sg>
2018-06-13 11:24:39 +0800

    Upload Log System Mk.IV Mod.IV
    Ref Id : 2018-0613-0123
    Desc : more pdf detail

--- app/main/logs/UploadLogsDetail.controller.js
+++ app/main/logs/UploadLogsDetail.controller.js
@@ -2,15 +2,18 @@
  * Created by shalitha on 17/5/16.
  */
 angular.module('itouch.controllers')
-  .controller('UploadLogsDetailCtrl', ['$log', 'Alert', '$localStorage', '$scope', '$rootScope', 'UploadService', '$state', '$cordovaEmailComposer',
-    function ($log, Alert, $localStorage, $scope, $rootScope, UploadService, $state, $cordovaEmailComposer) {
+  .controller('UploadLogsDetailCtrl', ['$log', 'Alert', '$localStorage', '$scope', '$rootScope', 'UploadService', '$state', '$cordovaEmailComposer', 'SettingsService',
+    function ($log, Alert, $localStorage, $scope, $rootScope, UploadService, $state, $cordovaEmailComposer, SettingsService) {
       var self = this;
-
+      var systemDate = new Date();
       // uploadLog = UploadService.StartuploadLog();
       $scope.upinterval = UploadService.getAutoUploadInterval();
       console.log($scope.upinterval);
       var uploadlog = localStorage.getItem('UploadLogs');
       // console.log(uploadlog);
+      if (uploadlog == null) {
+        uploadlog = 'no Data';
+      }
       $scope.newlogs = uploadlog;
       // $scope.newlogs = uploadLog.getLog();
       // console.log( $scope.newlogs);
@@ -43,14 +46,22 @@ angular.module('itouch.controllers')
         Alert.hideLoading();
       }
 
+      
       var lines = uploadlog.split('\n');
       var startline = 0;
       var lastline = 49
       var sline = lines.splice(startline,lastline);
       var first = sline.join('\n');
       var pdf = new jsPDF('p', 'pt', 'a4'), source  = "Text", margin = {top: 30, botton: 40, left: 30, width: 600};
+      pdf.setFontSize(12);
+      pdf.text("Upload Log" , 50, 30);
       pdf.setFontSize(10);
-      pdf.text( first, 50, 50 );
+      pdf.text("Location ID = " + SettingsService.getLocationId() + ", Machine ID = " + SettingsService.getMachineId(), 50, 45);
+      pdf.text("Entity ID = " + SettingsService.getEntityId() + ", Casher ID = " + SettingsService.getCashId(), 50, 60);
+      pdf.text("Business Date : " + SettingsService.getBusinessDate(), 50, 75);
+      pdf.text("System Date : " + systemDate , 50, 90);
+      pdf.setFontSize(10);
+      pdf.text( first, 50, 110 );
       var requirepages = lines.length/50;
       for (var i = 1; i < requirepages; i++, lastline+50, startline+50) {
         var sline = lines.splice(startline,lastline);
@@ -58,8 +69,14 @@ angular.module('itouch.controllers')
         pdf.addPage();
         pdf.text( newdata, 50, 50 );
       }
-      var pdfurl = pdf.output('dataurl');
-      // pdf.save('a4.pdf');
+      var pdfurl = pdf.output('dataurl'); 
+      // console.log(pdfurl);
+      pdf.save('a4.pdf');
+      
+      self.deleteManual = function() {
+        localStorage.removeItem('UploadLogs');
+        Alert.success('All Upload Log Delete  Successfully');
+      }
 
       self.sendMail = function(){
         cordova.plugins.email.isAvailable('gmail', function (hasAccount, hasGmail) {});
@@ -70,7 +87,7 @@ angular.module('itouch.controllers')
           //  bcc: , // email addresses for BCC field
            attachments: pdfurl,
            subject: "Upload Log <itouchlite>",
-           body: "<h1>Upload Log Attachment</h1>", 
+           body: "<h1>Upload Log Attachment</h1>" + "<p>Date : " + systemDate + "</p>", 
            isHtml: true 
          }).then(function () {
            console.log('email sent');
