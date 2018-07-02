0a4d4ec2a7c1781f99fb91ddbbe7c0e54b49c49e
Lynn Naing Zaw <lynnnaing@prima.com.sg>
2018-07-02 15:55:31 +0800

    Tally for BillHeader & BillDetail Mk.I
    Ref Id : 2018-0702-0142
    Desc : Tally for BillHeader & BillDetail

--- app/main/services/upload-serv.js
+++ app/main/services/upload-serv.js
@@ -190,6 +190,9 @@ angular.module('itouch.services')
       };
 
       var uploading = false;
+      var keyw = {'SubTotal': 0, 'DiscAmount': 0, 'Tax1Amount': 0, 'Tax1DiscAmount': 0, 'Tax2Amount': 0, 'Tax2DiscAmount': 0, 'Tax3Amount': 0, 'Tax3DiscAmount': 0, 'Tax4Amount': 0, 'Tax4DiscAmount': 0, 'Tax5Amount': 0, 'Tax5DiscAmount': 0};
+      var Headertotal = 0;
+      var Detailtotal = 0;
       self.upload = function () {
         uploadLog.clear();
         uploadLog.log('Uploading Status : ' + uploading, 2);
@@ -201,11 +204,38 @@ angular.module('itouch.services')
             var promises = [];  
             angular.forEach(bills, function (bill) {
               if (bill.BillDetail != undefined) {
+              // Headerkey = (({SubTotal, DiscAmount, Tax1Amount, Tax1DiscAmount, Tax2Amount, Tax2DiscAmount, Tax3Amount, Tax3DiscAmount, Tax4Amount, Tax4DiscAmount, Tax5Amount, Tax5DiscAmount}) => 
+              // ({SubTotal, DiscAmount, Tax1Amount, Tax1DiscAmount, Tax2Amount, Tax2DiscAmount, Tax3Amount, Tax3DiscAmount, Tax4Amount, Tax4DiscAmount, Tax5Amount, Tax5DiscAmount}))(bill.BillHeader);
+              Headertotal = (bill.BillHeader.SubTotal + bill.BillHeader.Tax1Amount + bill.BillHeader.Tax2Amount + bill.BillHeader.Tax3Amount + bill.BillHeader.Tax4Amount + bill.BillHeader.Tax5Amount) -
+              (bill.BillHeader.DiscAmount + bill.BillHeader.Tax1DiscAmount + bill.BillHeader.Tax2DiscAmount + bill.BillHeader.Tax3DiscAmount + bill.BillHeader.Tax4DiscAmount + bill.BillHeader.Tax5DiscAmount);
                 angular.forEach(bill.BillDetail, function (ids) {
                   console.log('Before : ' + ids.ItemId);
                   ids.ItemId = Math.floor(ids.ItemId);
                   console.log('After : '+ ids.ItemId);
+                  Object.keys(keyw).map(function(a){
+                    keyw [a] += ids [a];
+                  })
+                  Detailtotal = (keyw.SubTotal + keyw.Tax1Amount + keyw.Tax2Amount + keyw.Tax3Amount + keyw.Tax4Amount + keyw.Tax5Amount) -
+                  (keyw.DiscAmount + keyw.Tax1DiscAmount + keyw.Tax2DiscAmount + keyw.Tax3DiscAmount + keyw.Tax4DiscAmount + keyw.Tax5DiscAmount);
                 });
+                if (Headertotal == Detailtotal) {
+                  console.log(Headertotal);
+                  console.log(Detailtotal);
+                } else {
+                  bill.BillHeader.SubTotal = keyw.SubTotal;
+                  bill.BillHeader.DiscAmount = keyw.DiscAmount;
+                  bill.BillHeader.Tax1Amount = keyw.Tax1Amount;
+                  bill.BillHeader.Tax1DiscAmount = keyw.Tax1DiscAmount;
+                  bill.BillHeader.Tax2Amount = keyw.Tax2Amount;
+                  bill.BillHeader.Tax2DiscAmount = keyw.Tax2DiscAmount;
+                  bill.BillHeader.Tax3Amount = keyw.Tax3Amount;
+                  bill.BillHeader.Tax3DiscAmount = keyw.Tax3DiscAmount;
+                  bill.BillHeader.Tax4Amount = keyw.Tax4Amount;
+                  bill.BillHeader.Tax4DiscAmount = keyw.Tax4DiscAmount;
+                  bill.BillHeader.Tax5Amount = keyw.Tax5Amount;
+                  bill.BillHeader.Tax5DiscAmount = keyw.Tax5DiscAmount;
+                  console.log(bill.BillHeader);
+                }
               }
               if (typeof (bill.BillHeader) !== 'undefined') {
                 var DocNo = bill.BillHeader.DocNo;
@@ -227,7 +257,8 @@ angular.module('itouch.services')
                   console.log('UpdateVoidDocNo : ' + res);
                 }));
               }
-
+              keyw = {'SubTotal': 0, 'DiscAmount': 0, 'Tax1Amount': 0, 'Tax1DiscAmount': 0, 'Tax2Amount': 0, 'Tax2DiscAmount': 0, 'Tax3Amount': 0, 'Tax3DiscAmount': 0, 'Tax4Amount': 0, 'Tax4DiscAmount': 0, 'Tax5Amount': 0, 'Tax5DiscAmount': 0,};
+              // Headerkey = {};
                 /*
                 console.log("uploading bill");
                 console.log(bills);
