2ceeae58acbf37c42ede43109d2845c8f384909e
Lynn Naing Zaw <lynnnaing@prima.com.sg>
2018-07-05 17:02:43 +0800

    Log Support Mk.I (History)
    Ref Id : 2018-0705-0155
    Desc : Log Support Mk.I (History)

--- app/main/history/billView.controller.js
+++ app/main/history/billView.controller.js
@@ -2,8 +2,8 @@
  * Created by shalitha on 3/6/16.
  */
 angular.module('itouch.controllers')
-  .controller('BillViewCtrl', ['$scope', 'PrinterSettings', 'HistoryService', '$q', 'Reciept', 'LocationService', '$ionicScrollDelegate', 'DB', 'DB_CONFIG', 'BillService', 'SettingsService',
-    function ($scope, PrinterSettings, HistoryService, $q, Reciept, LocationService, $ionicScrollDelegate, DB, DB_CONFIG, BillService, SettingsService) {
+  .controller('BillViewCtrl', ['$scope', 'PrinterSettings', 'HistoryService', '$q', 'Reciept', 'LocationService', '$ionicScrollDelegate', 'DB', 'DB_CONFIG', 'BillService', 'LogService',
+    function ($scope, PrinterSettings, HistoryService, $q, Reciept, LocationService, $ionicScrollDelegate, DB, DB_CONFIG, BillService, LogService) {
       $scope.settings = {};
       $scope.bill = {
         header: null,
@@ -11,7 +11,8 @@ angular.module('itouch.controllers')
         transactions: null,
         discounts: null
       };
-      errorLog = SettingsService.StartErrorLog();
+      eventLog = LogService.StartEventLog();
+      errorLog = LogService.StartErrorLog();
       // errorLog.log(''+ err, 4);
       $scope.location = LocationService.currentLocation;
 
@@ -74,7 +75,7 @@ angular.module('itouch.controllers')
             var curtSysDateTime = moment().format('DD-MM-YYYY hh:mm:ss a');
             data.header.curtSysDateTime = curtSysDateTime;
           }, function (ex) {
-            errorLog.log('Bill View Error : '+ ex, 4);
+            errorLog.log('Bill View Error : ' + ex, 4);
             console.log(ex);
           });
         }
