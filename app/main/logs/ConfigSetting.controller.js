2edb9e64b7b427198c8d68266842c6a127e9be13
Lynn Naing Zaw <lynnnaing@prima.com.sg>
2018-08-21 16:53:02 +0800

    Config Mk.II
    Ref Id : 2018-0808-0198
    Desc : Config Mk.II

--- app/main/logs/ConfigSetting.controller.js
+++ app/main/logs/ConfigSetting.controller.js
@@ -2,8 +2,8 @@
  * Created by Lynn
  */
 angular.module('itouch.controllers')
-  .controller('ConfigSettingCtrl', ['Alert', '$localStorage', '$scope', '$rootScope', 'PrintService', '$state', '$ionicHistory',
-    function (Alert, $localStorage, $scope, $rootScope, PrintService, $state, $ionicHistory) {
+  .controller('ConfigSettingCtrl', ['Alert', '$localStorage', '$scope', '$rootScope', 'PrintService', '$state', '$ionicHistory', 'LogService', 'SettingsService', 'AppConfig', '$http',
+    function (Alert, $localStorage, $scope, $rootScope, PrintService, $state, $ionicHistory, LogService, SettingsService, AppConfig, $http) {
       var self = this;
 
       self.connect = function (ip, port) {
@@ -18,6 +18,34 @@ angular.module('itouch.controllers')
         Alert.hideLoading();
       };
       // console.log( $scope.newlogs);
+
+      $scope.DeleteAllFunction = function () {
+        var MachineId = SettingsService.getMachineId();
+        var requestUrl = AppConfig.getDisplayUrl() + '/Item';
+        $http({
+          method: 'DELETE',
+          url: requestUrl + '/' + MachineId,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        }).then(function successCallback (response) {
+          console.log('Delete');
+          console.log(response);
+          eventLog.log('2nd Display: Delete All Success');
+        }, function errorCallback (response) {
+          console.log(response);
+          errorLog.log('2nd Display Delete All Fail : ' + response.status + ' ' + response.statusText);
+        });
+      };
+
+      self.clearDisplay = function () {
+        $scope.DeleteAllFunction();
+        console.log('omae wa mo shide iru');
+      };
+
+      self.sendLog = function () {
+        LogService.PostLogData();
+      };
 
       self.Back = function () {
         var back = $ionicHistory.backView();
